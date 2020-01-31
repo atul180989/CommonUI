669e5d1e90bbac4c6dc68d74e7c4c0ed27523307
Atul Bhaisare <atul180989@gmail.com>
2020-01-31 00:52:29 -0600

    Initial First Commit

---
+++ MyCommonUI/MyCommonUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCommonUI
+//
+//  Created by Atul Bhaisare on 1/31/20.
+//  Copyright © 2020 Atul Bhaisare. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
